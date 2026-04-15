d5005739ff4d2cdc68e9ca6cd7ce82f21e484d06
mxggle <muggle6594@gmail.com>
2026-04-15 20:10:57 +0900

    fix: constrain layout height and enable internal scrolling to prevent page overflow

--- src/pages/PlayerPage.tsx
+++ src/pages/PlayerPage.tsx
@@ -73,7 +73,7 @@ export const PlayerPage = () => {
       setLayoutSettings={setLayoutSettings}
       bottomPaddingClassName="pb-28 sm:pb-28"
     >
-      <div className="flex flex-1 min-h-0 flex-col">
+      <div className="flex flex-1 min-h-0 flex-col h-full overflow-hidden">
         {/* Show loading message if media is being loaded */}
         {
           isLoadingMedia && (
